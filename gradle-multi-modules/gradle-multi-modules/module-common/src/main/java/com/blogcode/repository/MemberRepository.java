6fdc2aab98ad76cd1ec4fb0c9a3cb84bf470cfc9
devbelly <devbelly@naver.com>
2022-12-31 16:16:48 +0900

    multi module project + gradle

---
+++ gradle-multi-modules/gradle-multi-modules/module-common/src/main/java/com/blogcode/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.blogcode.repository;
+
+
+import com.blogcode.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
---
+++ gradle-multi-modules/gradle-multi-modules/module-web/src/main/java/com/blogcode/member/MemberController.java
@@ -0,0 +1,14 @@
+package com.blogcode.member;
+
+import com.blogcode.domain.Member;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+
+    @GetMapping("/")
+    public Member get(){
+        return new Member("jojoldu","jojoldu@gmail.com");
+    }
+}
