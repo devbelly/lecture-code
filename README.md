d09372298253b53db527f936b30374ddb47f90fe
devbelly <devbelly@naver.com>
2023-01-01 14:09:57 +0900

    예제 도메인 모델과 동작확인

---
+++ README.md
@@ -0,0 +1,25 @@
+## spring-data-jpa
+
+#### 2023-01-01
+
+- Entity는 `public`, `protected`를 접근제어자로 하는 기본생성자 필요
+
+  - JPA에서 객체에 값 매핑 시 기본 생성자로 객체를 생성 후 Reflection을 사용
+
+  - 지연로딩을 사용할 경우 프록시 객체 생성을 위해 기본 생성자 필요
+
+- `@SpringBootTest` 는 `@Transactional` 존재 시 기본적으로 Rollback
+
+- SQL parameter 확인하기
+
+  - `build.gradle`에 `implementation ‘com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.5.7’` 추가
+
+- 연관관계 설정 시 주의할 점
+
+  - 연관관계 주인? foregin key를 가지고 있는 쪽
+
+  - 연관관계 주인에 값을 입력하지 않으면 DB에 제대로 반영되지 않는다
+  - 객체 양방향 매핑을 하지 않으면 조회 시 문제 발생
+  - https://catsbi.oopy.io/ed9236a0-6521-471d-8a0d-b852147b5980
+
+- 모든 연관관계는 지연로딩으로 설정해야 최적화가 쉽다.
--- spring-boot-jpa/src/main/java/study/springbootjpa/entity/Member.java
+++ spring-boot-jpa/src/main/java/study/springbootjpa/entity/Member.java
@@ -1,28 +1,40 @@
 package study.springbootjpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@ToString(of={"id","username","age"})
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name="member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="team_id")
+    private Team team;
 
     public Member(String username){
         this.username=username;
     }
 
+    public Member(String member1, int age, Team teamA) {
+        this.username=member1;
+        this.age=age;
+        if(teamA!=null)
+            changeTeam(teamA);
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
