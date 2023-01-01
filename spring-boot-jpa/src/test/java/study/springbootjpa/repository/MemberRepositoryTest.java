22c33157d3b31903ef708739c5bd546babfd55b4
devbelly <devbelly@naver.com>
2023-01-01 12:44:58 +0900

    스프링 데이터 JPA와 DB설정, 동작확인

---
+++ spring-boot-jpa/src/test/java/study/springbootjpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package study.springbootjpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.springbootjpa.entity.Member;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember(){
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member member1 = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(member1.getId()).isEqualTo(member.getId());
+        assertThat(member1.getUsername()).isEqualTo(member.getUsername());
+    }
+}
