e6eabba663d7dcf4a1190c743b46c181f2bd84db
devbelly <devbelly@naver.com>
2023-01-05 15:10:40 +0900

    querydsl 초기설정

---
+++ spring-boot-querydsl/src/test/java/study/springbootquerydsl/SpringBootQuerydslApplicationTests.java
@@ -0,0 +1,40 @@
+package study.springbootquerydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.springbootquerydsl.entity.Hello;
+import study.springbootquerydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class SpringBootQuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+		Hello result = query.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+	}
+
+}
