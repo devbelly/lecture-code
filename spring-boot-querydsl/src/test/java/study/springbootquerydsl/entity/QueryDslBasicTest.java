45a00318e30291e614ec004e90284fb92a218f56
devbelly <devbelly@naver.com>
2023-01-06 13:53:29 +0900

    querydsl 집합

--- spring-boot-querydsl/src/test/java/study/springbootquerydsl/entity/QueryDslBasicTest.java
+++ spring-boot-querydsl/src/test/java/study/springbootquerydsl/entity/QueryDslBasicTest.java
@@ -1,6 +1,7 @@
 package study.springbootquerydsl.entity;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +16,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.springbootquerydsl.entity.QMember.member;
+import static study.springbootquerydsl.entity.QTeam.*;
 
 @SpringBootTest
 @Transactional
@@ -26,7 +28,7 @@ public class QueryDslBasicTest {
     JPAQueryFactory queryFactory;
 
     @BeforeEach
-    public void Before(){
+    public void Before() {
         queryFactory = new JPAQueryFactory(em);
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
@@ -34,11 +36,11 @@ public class QueryDslBasicTest {
         em.persist(teamA);
         em.persist(teamB);
 
-        Member member1 = new Member("member1",10,teamA);
-        Member member2 = new Member("member2",20,teamA);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
 
-        Member member3 = new Member("member3",30,teamB);
-        Member member4 = new Member("member4",40,teamB);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
         em.persist(member1);
         em.persist(member2);
         em.persist(member3);
@@ -46,7 +48,7 @@ public class QueryDslBasicTest {
     }
 
     @Test
-    public void startQuerydsl(){
+    public void startQuerydsl() {
 
 
         Member findMember = queryFactory
@@ -59,7 +61,7 @@ public class QueryDslBasicTest {
     }
 
     @Test
-    public void search(){
+    public void search() {
 
         Member findMember = queryFactory
                 .selectFrom(member)
@@ -73,7 +75,7 @@ public class QueryDslBasicTest {
     }
 
     @Test
-    public void result(){
+    public void result() {
 
         QueryResults<Member> results = queryFactory
                 .selectFrom(member)
@@ -84,11 +86,11 @@ public class QueryDslBasicTest {
     }
 
     @Test
-    public void sort(){
+    public void sort() {
 
-        em.persist(new Member("member5",100));
-        em.persist(new Member("member6",100));
-        em.persist(new Member(null,100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+        em.persist(new Member(null, 100));
 
         List<Member> result = queryFactory
                 .selectFrom(member)
@@ -98,5 +100,51 @@ public class QueryDslBasicTest {
         assertThat(result.get(0).getUsername()).isEqualTo("member5");
         assertThat(result.get(1).getUsername()).isEqualTo("member6");
         assertThat(result.get(2).getUsername()).isNull();
+    }
+
+    @Test
+    public void agg() {
+        List<Tuple> results = queryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = results.get(0);
+
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+    }
+    
+    @Test
+    public void group() throws Exception{
+        //given
+        List<Tuple> result = queryFactory
+                .select(
+                        team.name, member.age.avg()
+                )
+                .from(member)
+                .join(member.team, team)
+                .groupBy(member.team)
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+
+        //when
+        
+        //then    
     }
 }
